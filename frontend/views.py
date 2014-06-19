ebd6c4438842844b5abae378b3753a90665e7410
Diana Riscanevo <dianariscanevo@MacBook-Pro-de-Diana-Riscanevo.local>
2014-06-19 01:29:36 -0500

    Fixed fonts

--- frontend/views.py
+++ frontend/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, get_object_or_404
 from django.template import RequestContext
-from content.models import Article
+from content.models import *
 # Create your views here.
 
 TEMPLATE_DIR = 'frontend/../'
@@ -8,6 +8,9 @@ def home(request):
 
 	full_width_article = None
 	try:
+		categories = Category.objects.filter(parent=None)
+		print len(categories)
+
 		full_width_article = Article.objects.get(full_width=True)
 	except Exception, e:
 		pass
@@ -15,12 +18,12 @@ def home(request):
 	all_articles = Article.objects.exclude(full_width=True).order_by('-creation_date')
 	
 	return render_to_response(TEMPLATE_DIR+'site_index.html', 
-		{'full_width_article': full_width_article, 'articles':all_articles}, 
+		{'full_width_article': full_width_article, 'articles':all_articles, 'categories': categories}, 
 		context_instance=RequestContext(request)
 	)
 
 def serve_article(request, slug):
-	article = Article.objects.get(slug=slug)
+	article = get_object_or_404(Article, slug=slug)
 
 	print article
 
