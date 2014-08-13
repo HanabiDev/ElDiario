b22127993fbbbe0d94bc21634ebcc6d89069f9d6
Juan Gómez <gomexa@hotmail.com>
2014-08-13 01:18:54 -0500

    minor fix

--- mobile/urls.py
+++ mobile/urls.py
@@ -3,7 +3,7 @@ from django.contrib.staticfiles.urls import static
 from django.conf import settings
 
 urlpatterns = patterns('',
-	url(r'^$', 'mobile.views.home', name='mobile_home'),
+	url(r'^/$', 'mobile.views.home', name='mobile_home'),
 	url(r'^/articulos/$', 'mobile.views.all_articles', name='all_articles'),
 	url(r'^/articulos/(?P<slug>[-\w]+)/$', 'mobile.views.serve_article', name='serve_article'),
 	url(r'^/categorias/(?P<slug>[-\w]+)/$', 'mobile.views.serve_category', name='serve_category'),
